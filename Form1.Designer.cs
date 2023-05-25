d0159ee280b0a3f9c756407d06cf37b07613c2bc
ngynphu22 <ngynphu22@gmail.com>
2023-05-25 08:06:02 +0700

    HoaDon

---
+++ Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿
+namespace HoaDon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnThanhtoan = new System.Windows.Forms.Button();
+            this.btnQuaylai = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.WhiteSmoke;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(691, 370);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // btnThanhtoan
+            // 
+            this.btnThanhtoan.Location = new System.Drawing.Point(586, 481);
+            this.btnThanhtoan.Name = "btnThanhtoan";
+            this.btnThanhtoan.Size = new System.Drawing.Size(117, 25);
+            this.btnThanhtoan.TabIndex = 3;
+            this.btnThanhtoan.Text = "Thanh toán";
+            this.btnThanhtoan.UseVisualStyleBackColor = true;
+            this.btnThanhtoan.Click += new System.EventHandler(this.btnThanhtoan_Click);
+            // 
+            // btnQuaylai
+            // 
+            this.btnQuaylai.Location = new System.Drawing.Point(442, 479);
+            this.btnQuaylai.Name = "btnQuaylai";
+            this.btnQuaylai.Size = new System.Drawing.Size(108, 29);
+            this.btnQuaylai.TabIndex = 4;
+            this.btnQuaylai.Text = "Quay lại";
+            this.btnQuaylai.UseVisualStyleBackColor = true;
+            this.btnQuaylai.Click += new System.EventHandler(this.btnQuaylai_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.ClientSize = new System.Drawing.Size(715, 518);
+            this.Controls.Add(this.btnQuaylai);
+            this.Controls.Add(this.btnThanhtoan);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Hóa đơn";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnThanhtoan;
+        private System.Windows.Forms.Button btnQuaylai;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace HoaDon
+{
+    public partial class Form1 : Form
+    {
+        string chuoiKetnoi = @"Data Source=TUF\SQLEXPRESS;Initial Catalog=DonHang;Integrated Security=True";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void load()
+        {
+            SqlConnection conn = new SqlConnection(chuoiKetnoi);
+
+            try
+            {
+                conn.Open();
+                string sql = "select * from DonHang";
+                SqlDataAdapter dt = new SqlDataAdapter(sql, conn);
+                DataTable tb = new DataTable();
+                dt.Fill(tb);
+                dataGridView1.DataSource = tb;
+
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi kết nối:" + ex.Message);
+            }
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            load();
+        }
+
+        private void btnThanhtoan_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Thanh toán thành công", "Thông báo");
+            load();
+        }
+
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnQuaylai_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
